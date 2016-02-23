e36a793040a156bdd43417699a4caee95ef62423
Stefan Stoican <sstoi001@gold.ac.uk>
2016-02-23 18:54:49 +0000

    trying to tilt heads

--- Assets/GazeFollowing.cs
+++ Assets/GazeFollowing.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class GazeFollowing : MonoBehaviour {
 
-    GameObject Gaze, Agent;
+    public bool overwrite;
+    GameObject Gaze, Agent, Clone;
+    float angle;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+      //  Clone = (GameObject) Instantiate(gameObject, gameObject.transform.position, Quaternion.Euler(0, angle, 0));
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -16,10 +18,25 @@ public class GazeFollowing : MonoBehaviour {
 
         Gaze = GameObject.FindGameObjectWithTag("Gaze");
 
-        if ( Vector3.Distance(Agent.transform.position, Gaze.transform.position) <=3)
+      //  Gaze.transform.rotation = Random.rotation;
+
+        if ( Vector3.Distance(Agent.transform.position, Gaze.transform.position) <= 7)
          {
-            Debug.DrawLine(Agent.transform.position, Gaze.transform.position, Color.red,1000, false); 
-         }
+            angle = Vector3.Angle(Gaze.transform.position - Agent.transform.position, Agent.transform.forward);
+
+            //Clone = Instantiate(gameObject, gameObject.transform.position, Quaternion.Euler(0, angle, 0) );
+            // GameObject.Find(name).transform.rotation = Quaternion.Euler(Gaze.transform.position);
+            //Debug.Log(angle);
+            //gameObject.transform.localScale = Gaze.transform.position;
+            // Clone.transform.position = gameObject.transform.position;
+            //   Clone.transform.forward = Gaze.transform.position - Clone.transform.position;
+            //localrotation - relativ4e to the parent's rotation
+            gameObject.transform.forward = Gaze.transform.position - gameObject.transform.position;
+
+
+
+           //  Debug.DrawLine(Agent.transform.position, Gaze.transform.position, Color.red,1000, true); 
+        }
 
 	}
 }
