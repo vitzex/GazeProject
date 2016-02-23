bc9a1982b6985d431dea1ed60b11bb53f05f9623
Stefan Stoican <sstoi001@gold.ac.uk>
2016-02-23 17:14:21 +0000

    linking up gazes

---
+++ Assets/GazeFollowing.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeFollowing : MonoBehaviour {
+
+    GameObject Gaze, Agent;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Agent = gameObject; //the object being called
+
+        Gaze = GameObject.FindGameObjectWithTag("Gaze");
+
+        if ( Vector3.Distance(Agent.transform.position, Gaze.transform.position) <=3)
+         {
+            Debug.DrawLine(Agent.transform.position, Gaze.transform.position, Color.red,1000, false); 
+         }
+
+	}
+}
