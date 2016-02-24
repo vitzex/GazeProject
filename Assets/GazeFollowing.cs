5d1d34a518ffbf57ddff9147929ce6b04103b9e8
Stefan Stoican <sstoi001@gold.ac.uk>
2016-02-24 15:31:59 +0000

    Before steer attempt

--- Assets/GazeFollowing.cs
+++ Assets/GazeFollowing.cs
@@ -13,6 +13,11 @@ public class GazeFollowing : MonoBehaviour {
         Agent = gameObject; //the object being called
 
         Gaze = GameObject.FindGameObjectWithTag("Gaze");
+        Gaze.transform.parent.transform.localRotation = Quaternion.Euler(Random.Range(0, 80), Random.Range(-80, 80), Random.Range(0, 80));
+        //can rotate -80 to 80 sideways (y rotation
+        // can rotate 0 to 80 up-down (x, z rotation) so that others can stare up too (not be blocked)
+        //only works when joint_head copied outside of neck -> parent now torso
+
         transferThreshold = 7;
 
         threshold = GameObject.CreatePrimitive(PrimitiveType.Sphere);
@@ -32,7 +37,15 @@ public class GazeFollowing : MonoBehaviour {
 
         if (Vector3.Distance(Agent.transform.position, Gaze.transform.parent.transform.position) <= transferThreshold)
         {
-            angle = Vector3.Angle(Gaze.transform.position - Agent.transform.position, Agent.transform.forward);
+            //    angle = Vector3.Angle(Gaze.transform.position - Agent.transform.position, Agent.transform.forward);
+
+            //   Vector3 dist = Gaze.transform.position - Gaze.transform.position - gameObject.transform.position;
+            //     Vector3 desired = dist.normalized * maxSpeed;
+            //     gameObject.transform.forward = desired; // - gameObject.transform.forward; //forward is basically our velocity
+
+            /////STUFF BEFORE
+
+            gameObject.transform.forward = Gaze.transform.position - gameObject.transform.position;
 
             //Clone = Instantiate(gameObject, gameObject.transform.position, Quaternion.Euler(0, angle, 0) );
             // GameObject.Find(name).transform.rotation = Quaternion.Euler(Gaze.transform.position);
@@ -41,7 +54,7 @@ public class GazeFollowing : MonoBehaviour {
             // Clone.transform.position = gameObject.transform.position;
             //   Clone.transform.forward = Gaze.transform.position - Clone.transform.position;
             //localrotation - relativ4e to the parent's rotation  
-            gameObject.transform.forward = Gaze.transform.position - gameObject.transform.position;
+
             //  Debug.DrawLine(Agent.transform.position, Gaze.transform.position, Color.red,1000, true); 
         }
         else gameObject.transform.forward = gameObject.transform.parent.transform.forward; //chest forward
