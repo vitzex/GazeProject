39b5824e4079007454073bcb1b69ff5e49607945
Stefan Stoican <sstoi001@gold.ac.uk>
2016-02-24 13:29:44 +0000

    threshold around gazer

--- Assets/GazeFollowing.cs
+++ Assets/GazeFollowing.cs
@@ -4,23 +4,33 @@ using System.Collections;
 public class GazeFollowing : MonoBehaviour {
 
     public bool overwrite;
-    GameObject Gaze, Agent, Clone;
-    float angle;
+    GameObject Gaze, Agent, Clone, threshold;
+    float angle, transferThreshold;
 
 	// Use this for initialization
 	void Start () {
-      //  Clone = (GameObject) Instantiate(gameObject, gameObject.transform.position, Quaternion.Euler(0, angle, 0));
+
+        Agent = gameObject; //the object being called
+
+        Gaze = GameObject.FindGameObjectWithTag("Gaze");
+        transferThreshold = 7;
+
+        threshold = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        threshold.GetComponent<Collider>().enabled = false;
+        threshold.transform.position = Gaze.transform.parent.transform.position - new Vector3(0,Gaze.transform.parent.transform.position.y, 0);
+        threshold.transform.localScale = new Vector3(2*transferThreshold, 0.1f, 2*transferThreshold);
+
+        
+
+        //  Clone = (GameObject) Instantiate(gameObject, gameObject.transform.position, Quaternion.Euler(0, angle, 0));
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Agent = gameObject; //the object being called
-
-        Gaze = GameObject.FindGameObjectWithTag("Gaze");
-
+        threshold.transform.position = Gaze.transform.parent.transform.position - new Vector3(0, Gaze.transform.parent.transform.position.y, 0); //in case gaze.parent moves
         //  Gaze.transform.rotation = Random.rotation;
 
-        if (Vector3.Distance(Agent.transform.position, Gaze.transform.position) <= 7)
+        if (Vector3.Distance(Agent.transform.position, Gaze.transform.parent.transform.position) <= transferThreshold)
         {
             angle = Vector3.Angle(Gaze.transform.position - Agent.transform.position, Agent.transform.forward);
 
